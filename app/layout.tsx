e8340a16dffbdc9399fb9a5ebc450b99c4ac5068
Hikaru827 <hikarujin167@gmail.com>
2026-04-08 23:42:29 +0900

    fix:アイコン

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/icon-light-32x32.png", media: "(prefers-color-scheme: light)" },
       { url: "/icon-dark-32x32.png", media: "(prefers-color-scheme: dark)" },
-      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/icon.png", sizes: "512x512", type: "image/png" },
     ],
     apple: "/apple-icon.png",
   },
